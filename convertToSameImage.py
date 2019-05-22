90495a6c8d290f120abe85be19050c47ce614663
unknown <jackellis.home@gmail.com>
2019-05-22 13:52:54 -0400

    Changed Cloud2 data

--- convertToSameImage.py
+++ convertToSameImage.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 
 old_dir = 'data\cloud1'
 new_dir = 'data\cloud2'
-filename = 'pic0.png'
+filename1 = 'pic0.png'
+filename2 = 'pic3.png'
 
 for i in range(len(os.listdir(old_dir))):
-    path = os.path.join(old_dir,filename)
-    im = cv2.imread(path,cv2.IMREAD_UNCHANGED)
-    newPath = os.path.join(new_dir,'pic{}.png'.format(i))
-    cv2.imwrite(newPath,im)
+    if i<25:
+        path = os.path.join(old_dir,filename1)
+        im = cv2.imread(path,cv2.IMREAD_UNCHANGED)
+        newPath = os.path.join(new_dir,'pic{}.png'.format(i))
+        cv2.imwrite(newPath,im)
+    else:
+        path = os.path.join(old_dir,filename2)
+        im = cv2.imread(path,cv2.IMREAD_UNCHANGED)
+        newPath = os.path.join(new_dir,'pic{}.png'.format(i))
+        cv2.imwrite(newPath,im)
